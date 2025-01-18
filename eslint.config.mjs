bb719f2c940b57ee61b4f1b24a7846ff1b21565c
Jenny Melnychenko <melnichenkojenny@gmail.com>
2025-01-18 14:14:21 +0200

    [CONFIG] Add prettier and lint

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,16 +1,40 @@
-import { dirname } from "path";
-import { fileURLToPath } from "url";
-import { FlatCompat } from "@eslint/eslintrc";
+import { dirname } from 'path'
+import { fileURLToPath } from 'url'
+import { FlatCompat } from '@eslint/eslintrc'
 
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = dirname(__filename);
+const __filename = fileURLToPath(import.meta.url)
+const __dirname = dirname(__filename)
 
 const compat = new FlatCompat({
   baseDirectory: __dirname,
-});
+})
 
 const eslintConfig = [
-  ...compat.extends("next/core-web-vitals", "next/typescript"),
-];
+  ...compat.extends('next/core-web-vitals', 'next/typescript'),
+  {
+    rules: {
+      'arrow-body-style': ['error', 'as-needed'],
+      'max-len': [
+        'error',
+        { code: 80, ignoreRegExpLiterals: true, ignoreStrings: true },
+      ],
+      'no-console': [
+        'error',
+        { allow: ['debug', 'error', 'info', 'trace', 'warn'] },
+      ],
+      'object-shorthand': ['error', 'always'],
+      'quote-props': ['warn', 'consistent-as-needed'],
+      'quotes': [
+        'error',
+        'single',
+        { allowTemplateLiterals: true, avoidEscape: true },
+      ],
+      'react/jsx-curly-brace-presence': ['error', 'always'],
+      'react/self-closing-comp': ['error', { component: true, html: true }],
+      'react-hooks/rules-of-hooks': 'error',
+      'react-hooks/exhaustive-deps': 'warn',
+    },
+  },
+]
 
-export default eslintConfig;
+export default eslintConfig
