7ab1ff1aa800dcab2c63f7492ddeb06488273a20
Jenny Melnychenko <melnichenkojenny@gmail.com>
2025-01-18 19:35:40 +0200

    Update FilterPage (HOME)

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,24 +1,15 @@
-export default function Home() {
+'use client'
+
+import FilterSection from './components/FilterSection'
+
+export default function FilterPage() {
   return (
-    <div
+    <main
       className={
-        'grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]'
+        'min-h-screen bg-gradient-to-t from-blue-50 via-blue-100 to-blue-200 flex justify-center p-2'
       }
     >
-      <main
-        className={
-          'flex flex-col gap-8 row-start-2 items-center sm:items-start'
-        }
-      >
-        <p>{'✨ Home Page 🐱‍🚀'}</p>
-      </main>
-      <footer
-        className={
-          'row-start-3 flex gap-6 flex-wrap items-center justify-center'
-        }
-      >
-        <p>{'✨Footer🙌'}</p>
-      </footer>
-    </div>
+      <FilterSection />
+    </main>
   )
 }
